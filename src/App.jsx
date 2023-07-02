99adb21ece04d8ff3c5acc7197ceca1d1d62441f
Prakash <kafleprakash0@gmail.com>
2023-07-02 20:00:49 +0545

    react starter template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Starter from "./components/starter"
+
+function App() {
+  return (
+    <>
+      <Starter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/starter.jsx
@@ -0,0 +1,7 @@
+const Starter = () => {
+    return (
+        <div className="text-blue-500 font-bold text-center">Template Starter</div>
+    )
+}
+
+export default Starter
